8a78fa06d45b8c53a2439e158574211174eeaf47
ymegane <ymegane.airhole+github@gmail.com>
2017-01-28 15:08:53 +0900

    :memo: bump version to 1.0.0

--- src/main/groovy/com/github/ymegane/gradle/balto/plugins/Config.groovy
+++ src/main/groovy/com/github/ymegane/gradle/balto/plugins/Config.groovy
@@ -1,7 +1,7 @@
 package com.github.ymegane.gradle.balto.plugins
 
 class Config {
-    static final def VERSION = '0.1.2'
+    static final def VERSION = '1.0.0'
     static final def USER_AGENT = "gradle-balto-upload-plugin/${VERSION}"
     static final def BALTO_ROOT = 'https://balto-api.herokuapp.com'
 }
